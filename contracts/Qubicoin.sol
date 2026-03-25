c9543b72df1885f7c672d75d90192f72e73ad45b
Sovereign Titan Genesis <admin@quorumstate.international>
2026-03-25 15:18:51 -0400

    Update Qubicoin.sol

--- contracts/Qubicoin.sol
+++ contracts/Qubicoin.sol
@@ -1,1 +1,6 @@
+// Tambahkan modifier agar hanya kontrak Governance yang bisa memicu aksi besar
+modifier onlyTitanGovernance() {
+    require(msg.sender == titanGovernanceAddress, "Hanya Titan Governance yang punya otoritas");
+    _;
+}
 
