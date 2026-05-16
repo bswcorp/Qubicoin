86686678f2f3347a5773e22c50b13875ee3cf028
Quorum-State <admin@quorumstate.international>
2026-05-16 19:11:55 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,32 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 module.exports = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.20",
   networks: {
-    // Jaringan Polygon Amoy (Hemat & Cepat)
-    amoy: {
-      url: "https://rpc-amoy.polygon.technology",
-      accounts: [process.env.PRIVATE_KEY],
+    hardhat: {
+      chainId: 8081
     },
-    // Jaringan Base Sepolia (Ekosistem Coinbase)
-    base_sepolia: {
-      url: "https://sepolia.base.org",
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    // Untuk verifikasi kontrak gratis agar transparan
-    apiKey: {
-      polygonAmoy: process.env.POLYGONSCAN_API_KEY,
-    },
-  },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 8081
+    }
+  }
 };
