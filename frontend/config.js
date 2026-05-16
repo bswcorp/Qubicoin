19a82453640b3cc1fd89884d94b994895451ad69
Quorum-State <admin@quorumstate.international>
2026-05-16 19:09:47 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,8 @@
+export const CONFIG = {
+  chainId: 8081,
+  chainHex: "0x1F91",
+  rpc: "http://127.0.0.1:8545",
+  explorer: "http://localhost:4000",
+  contract: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+};
 
